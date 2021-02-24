002d2d1864e622f0134f43e76ebcd252bb59f8f3
Ceci <liuxiyi@hotmail.com>
2021-02-24 15:23:44 -0500

    Add payment method page

---
+++ src/components/pages/PaymentMethod.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { Tabs, Tab, Row, Col, Button} from 'react-bootstrap';
+import Paper from '@material-ui/core/Paper';
+import CarSelectionSidebar from '../CarSelectionSidebar';
+
+//icons
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+
+class PaymentMethod extends Component {
+    state = {  };
+
+    render() { 
+        const cashAndFinance = {
+            backgroundColor: "#e9ecef",
+            display: 'flex',
+            flexDirection: 'column',
+            paddingLeft: 32,
+            paddingRight: 32,
+            paddingTop: 10
+        };
+
+        const cardStyle = {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderRadius: 10,
+            padding: 20,
+            // borderStyle: "solid",
+            // borderWidth: 3,
+            // borderColor: "#009de0",
+            height: 470,
+            width: 370,
+            marginLeft: 16,
+            marginRight: 16
+        }
+        return (
+            <div style={{ paddingTop: 20 }}>
+                <div>
+                    <Tabs defaultActiveKey="cash-finance">
+                        <Tab eventKey="trade-in" title="1. Trade-in" ></Tab>
+                        <Tab eventKey="cash-finance" title="2. Cash/Finance"></Tab>
+                        <Tab eventKey="delivery-methods" title="3. Delievery method" disabled></Tab>
+                        <Tab eventKey="review-payment" title="4. Review & Payment" disabled></Tab>
+                    </Tabs>
+                </div>
+
+                <Row className="body-wrapper">
+                    <Col md={9} style={cashAndFinance}>
+                        <div style={{ fontSize: 14 }}>What is your payment method? </div>
+                        <div style={{ display: 'flex', justifyContent: 'center', height: "100vh"}}>
+                            <Paper
+                            elevation={3}
+                            style={cardStyle}>
+                                <div>
+                                    <div style={{display:"flex", justifyContent:"center", marginBottom: 16}}>
+                                        <AccountBalanceIcon style={{fontSize: 90}}/> 
+                                    </div>
+                                    <div>
+                                        <h4 style={{textAlign: "center", fontSize: 22}}>Finance with us</h4>
+                                    </div>
+                                    <div>
+                                        <div style={{ marginBottom: 10}}>
+                                            <span style={{fontSize: 14}}>Eligible for no payments for up to 90 days</span>
+                                        </div>
+                                        <div style={{ marginBottom: 10 }}>
+                                            <span style={{ fontSize: 14 }}>Loans provided by Canada's largest banks</span>
+                                        </div>
+                                        <div style={{ marginBottom: 10 }}>
+                                            <span style={{ fontSize: 14 }}>Lowest interest rates available</span>
+                                        </div>
+                                        <div style={{ marginBottom: 10 }}>
+                                            <span style={{ fontSize: 14 }}>Customize your loan's terms from home</span>
+                                        </div>
+                                        <div style={{ marginBottom: 10 }}>
+                                            <span style={{ fontSize: 14 }}>Payback in full at any time with no penalty</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br/>
+                                <div>
+                                    <Button 
+                                    variant="primary"
+                                    size="lg">
+                                    Continue with Finance
+                                    </Button>
+                                </div>
+                            </Paper>
+                            <Paper
+                            elevation={3}
+                            style={cardStyle}>
+                                <div>
+                                    <div style={{ display: "flex", justifyContent: "center", marginBottom: 16 }}>
+                                        <MonetizationOnIcon style={{ fontSize: 90 }} />
+                                    </div>
+                                    <div>
+                                        <h4 style={{ textAlign: "center", fontSize: 22 }}>Pay with cash</h4>
+                                    </div>
+                                    <div>
+                                        <div style={{ marginBottom: 10 }}>
+                                            <span style={{ fontSize: 14 }}>Payment by certified cheque or bank draft</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br />
+                                <div>
+                                    <Button
+                                    variant="outline-primary"
+                                    size="lg">
+                                    Continue with Cash
+                                    </Button>
+                                </div>
+                            </Paper>
+                        </div>
+                        
+                    </Col>
+                    <Col>
+                        <CarSelectionSidebar />
+                    </Col>
+                </Row>
+            </div>
+          );
+    }
+}
+ 
+export default PaymentMethod;
