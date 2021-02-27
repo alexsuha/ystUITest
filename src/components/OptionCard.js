fe8e00e2dee22bc86aba86fa56a550e6ce6dfc2c
Ceci <liuxiyi@hotmail.com>
2021-02-27 18:30:50 -0500

    Add option card with apis

---
+++ src/components/OptionCard.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { Paper, makeStyles } from '@material-ui/core';
+import { Col, Button } from 'react-bootstrap';
+
+const useStyles = makeStyles((theme) => ({
+    col: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+
+    optionLabel: {
+        height: 34,
+        width: 270,
+        display: "flex",
+        justifyContent: "center",
+        margin: "0 50px 10px",
+        padding: "5px 10px",
+        borderRadius: 10,
+        opacity: 1,
+        border: "1px solid #009de0"
+    },
+
+    labelText: {
+        fontWeight: "bold",
+        fontSize: 14,
+        textAlign: "center",
+        color: "#009de0"
+    },
+
+    card: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderRadius: 10,
+        padding: 20,
+        height: 470,
+        width: 370,
+        marginLeft: 16,
+        marginRight: 16
+    },
+
+    highlitedCard: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderRadius: 10,
+        padding: 20,
+        borderStyle: "solid",
+        borderWidth: 3,
+        borderColor: "#009de0",
+        height: 470,
+        width: 370,
+        marginLeft: 16,
+        marginRight: 16
+    },
+
+    iconContainer: {
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: 16,
+    },
+
+    description: {
+        marginBottom: 10
+    }
+
+}));
+
+
+function OptionCard(props) {
+    const classes = useStyles();
+    const { title, description, buttonText, icon, onClick, showLabel } = props;
+
+    return <Col className={classes.col}>
+        {showLabel && <Paper
+            elevation={0}
+            className={classes.optionLabel}
+        >
+            <span className={classes.labelText}>Most popular</span>
+        </Paper>
+        }
+
+        {!showLabel && <Paper
+            elevation={0}
+            className={classes.optionLabel}
+            style={{ visibility: "hidden" }}>
+            <span className={classes.labelText}>Most popular</span>
+        </Paper>
+        }
+        <Paper
+            elevation={3}
+            variant='outlined'
+            className={showLabel ? classes.highlitedCard : classes.card}
+        >
+            <div>
+                <div className={classes.iconContainer}>
+                    {icon}
+                </div>
+                <div>
+                    <h4 style={{ textAlign: "center", fontSize: 22 }}>{title}</h4>
+                </div>
+                <div>
+                    {description.map((item) => (
+                        <div className={classes.description}>
+                            <span key={item.id} style={{ fontSize: 14 }}>{item.value}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <br />
+            <div>
+                <Button
+                    variant="primary"
+                    size="lg"
+                    onClick={onClick}>
+                    {buttonText}
+                </Button>
+            </div>
+        </Paper>
+    </Col>
+
+
+}
+
+export default OptionCard;
--- src/components/pages/PaymentMethod.js
+++ src/components/pages/PaymentMethod.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Row, Col, Button} from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import Paper from '@material-ui/core/Paper';
+
+//components
+import OptionCard from '../OptionCard';
 import CarSelectionSidebar from '../CarSelectionSidebar';
 import ServiceTabs from '../ServiceTabs';
 
@@ -14,7 +17,7 @@ class PaymentMethod extends Component {
         super(props);
     }
 
-    render() { 
+    render() {
         const cashAndFinance = {
             backgroundColor: "#e9ecef",
             display: 'flex',
@@ -31,97 +34,61 @@ class PaymentMethod extends Component {
             alignItems: "center",
             borderRadius: 10,
             padding: 20,
-            // borderStyle: "solid",
-            // borderWidth: 3,
-            // borderColor: "#009de0",
+            borderStyle: "solid",
+            borderWidth: 3,
+            borderColor: "#009de0",
             height: 470,
             width: 370,
             marginLeft: 16,
             marginRight: 16
         }
+
+        const financeDescription = [
+            { id: 0, value: "Eligible for no payments for up to 90 days" },
+            { id: 1, value: "Loans provided by Canada's largest banks" },
+            { id: 2, value: "Lowest interest rates available" },
+            { id: 3, value: "Customize your loan's terms from home" },
+            { id: 4, value: "Payback in full at any time with no penalty" }
+        ]
+
+        const cashDescription = [
+            { id: 0, value: "Payment by certified cheque or bank draft" }
+        ]
         return (
             <div>
-                <ServiceTabs 
+                <ServiceTabs
                     tabValue={this.props.tabValue}
                     onTabChange={this.props.onTabChange}
                 />
                 <Row>
                     <Col md={9} style={cashAndFinance}>
-                        <div style={{ fontSize: 14 }}>What is your payment method? </div>
-                        <div style={{ display: 'flex', justifyContent: 'center', height: "100vh"}}>
-                            <Paper
-                            elevation={3}
-                            style={cardStyle}>
-                                <div>
-                                    <div style={{display:"flex", justifyContent:"center", marginBottom: 16}}>
-                                        <AccountBalanceIcon style={{fontSize: 90}}/> 
-                                    </div>
-                                    <div>
-                                        <h4 style={{textAlign: "center", fontSize: 22}}>Finance with us</h4>
-                                    </div>
-                                    <div>
-                                        <div style={{ marginBottom: 10}}>
-                                            <span style={{fontSize: 14}}>Eligible for no payments for up to 90 days</span>
-                                        </div>
-                                        <div style={{ marginBottom: 10 }}>
-                                            <span style={{ fontSize: 14 }}>Loans provided by Canada's largest banks</span>
-                                        </div>
-                                        <div style={{ marginBottom: 10 }}>
-                                            <span style={{ fontSize: 14 }}>Lowest interest rates available</span>
-                                        </div>
-                                        <div style={{ marginBottom: 10 }}>
-                                            <span style={{ fontSize: 14 }}>Customize your loan's terms from home</span>
-                                        </div>
-                                        <div style={{ marginBottom: 10 }}>
-                                            <span style={{ fontSize: 14 }}>Payback in full at any time with no penalty</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <br/>
-                                <div>
-                                    <Button 
-                                        variant="primary"
-                                        size="lg">
-                                        Continue with Finance
-                                    </Button>
-                                </div>
-                            </Paper>
-                            <Paper
-                                elevation={3}
-                                style={cardStyle}>
-                                <div>
-                                    <div style={{ display: "flex", justifyContent: "center", marginBottom: 16 }}>
-                                        <MonetizationOnIcon style={{ fontSize: 90 }} />
-                                    </div>
-                                    <div>
-                                        <h4 style={{ textAlign: "center", fontSize: 22 }}>Pay with cash</h4>
-                                    </div>
-                                    <div>
-                                        <div style={{ marginBottom: 10 }}>
-                                            <span style={{ fontSize: 14 }}>Payment by certified cheque or bank draft</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <br />
-                                <div>
-                                    <Button
-                                        variant="outline-primary"
-                                        size="lg"
-                                        onClick={this.props.nextStep}>
-                                        Continue with Cash
-                                    </Button>
-                                </div>
-                            </Paper>
+                        <div style={{ fontSize: 14, marginBottom: 8 }}>What is your payment method? </div>
+                        <div style={{ display: 'flex', justifyContent: 'center', height: "100vh" }}>
+                            <OptionCard
+                                icon={<AccountBalanceIcon style={{ fontSize: 90 }} />}
+                                title="Finance with us"
+                                description={financeDescription}
+                                buttonText="Continue with finance"
+                                onClick={this.props.nextStep}
+                                showLabel={true}
+                            />
+                            <OptionCard
+                                icon={<MonetizationOnIcon style={{ fontSize: 90 }} />}
+                                title="Pay with cash"
+                                description={cashDescription}
+                                buttonText="Continue with cash"
+                                onClick={this.props.nextStep}
+                                showLabel={false}
+                            />
                         </div>
-                        
                     </Col>
                     <Col>
                         <CarSelectionSidebar />
                     </Col>
                 </Row>
-            </div>
-          );
+            </div >
+        );
     }
 }
- 
+
 export default PaymentMethod;
